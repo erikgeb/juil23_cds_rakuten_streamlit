b551d54d6535dc5a0c7b8abf4ac05bf374e8cf7e
Erik Gebers <erik.gebers@gmail.com>
2024-05-12 17:50:25 +0200

    Add random loading of test data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 def colored_header(label, description=None, color_code="#803df5"):
     """Shows a header with a colored underline and an optional description."""
@@ -17,28 +18,36 @@ def icon(emoji: str = "📊"):
         unsafe_allow_html=True,
     )
 
-
-#st.title("Projet de classification de produits dans des catégories")
 icon()
 colored_header("Classification automatique de produits dans des catégories", "Projet Rakuten challenge, il s'agit de pouvoir classifier des annonces dans des catégories de produit en fonction de leur titre, description textuelle et image.")
 st.sidebar.title("Sommaire")
-pages=["Introduction", "Exploration", "DataVizualization", "Modélisation"]
+pages=["Introduction", "DataVizualization", "Modélisation"]
 page=st.sidebar.radio("Aller vers", pages)
+X_test=pd.read_csv('./resources/X_test_update.csv', index_col=0).fillna("")
 
 if page == pages[0] : 
   st.write("### Introduction")
-
-  st.code("exampleCode = \"this is an example\"")
-  st.code("exampleCode = exampleCode + \".\"")
-
+  st.write("Exemple d'image produit")
   st.image("https://fr.shopping.rakuten.com/photo/web-app-development-made-simple-with-streamlit-format-poche-10412352030_L_NOPAD.jpg")
 
 if page == pages[1] : 
-  st.write("### Exploration")
+  st.write("### DataVizualization")
+  X_test=pd.read_csv('./resources/X_test_update.csv', index_col=0)
+  X_train=pd.read_csv('./resources/X_train_update.csv', index_col=0)
+  Y_train=pd.read_csv('./resources/Y_train_CVw08PX.csv', index_col=0)
 
 if page == pages[2] : 
-  st.write("### DataVizualization")
-
-if page == pages[3] : 
   st.write("### Modélisation")
+  st.write("Initialisation avec des valeurs de test aléatoires :")
+  X_test_random = X_test.sample().iloc[0]
+  designation = st.text_input("Designation", value=X_test_random['designation'], key="designation")
+  st.text_area("Description",
+   value=X_test_random['description'])
+  st.text_input("Image URL", "https://www.example.com")
 
+  col1, col2 = st.columns(2)
+  if col1.button("Use another random test data entry"):
+    # The simple click of the button will redraw UI and select another random item
+    useless = None
+
+  col2.button("Predict class")
